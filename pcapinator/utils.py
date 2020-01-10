0f4aae19dc04e3bfb05db02125107434245ac029
Ivan Tyshchenko <iv.tihon@gmail.com>
2020-01-10 19:53:26 +0200

    Move method convert2hccapx into captools

--- pcapinator/utils.py
+++ pcapinator/utils.py
@@ -1,10 +1,11 @@
 import logging
 import struct
+import subprocess
 from functools import partial, update_wrapper, wraps
 from itertools import chain
 from pathlib import Path
 from time import perf_counter
-from typing import List, Text, Generator
+from typing import List, Iterator, Text, Generator
 from uuid import uuid4
 
 import pandas as pd
@@ -12,6 +13,7 @@ import pandas as pd
 from pcapinator.consts import CHUNKSZ
 
 global DEBUG_LEVEL
+
 
 perf_logger = logging.getLogger('perf')
 util_logger = logging.getLogger('util')
@@ -56,6 +58,8 @@ def fix_broken_ssid_line(s: Text) -> Text:
     :param s: broken string
 
     :return fixed string
+
+    TODO: Add the ability to call this function from the command line, test for functionality
     """
 
     def _find_nth(haystack, needle, n):
@@ -121,9 +125,9 @@ pd_chunked_reader = update_wrapper(
 
 @add_perf_counter
 def make_unique_ssid_for_tsv(
-        file: Path,
+        file: Iterator[Path],
         chunksize=CHUNKSZ
-) -> Generator[Path, None, None]:
+) -> Path:
     """
     Function for creating small temporary TSV pieces with unique values
     :return: TSV with uq-data
@@ -172,3 +176,55 @@ def write_pcap_packet(f, timeval_s, timeval_us, packet_bytes):
     )
     f.write(pkt)
     f.write(packet_bytes)
+
+
+@add_perf_counter
+def convert2hccapx(*args, out_dir: Path, executable: Path, files: Iterator[Path] = None):
+    @add_perf_counter
+    def _call_converter(file: Path):
+        _cmd = '"{}" "{}" "{}.hccapx"'.format(
+            executable,
+            file.resolve(),
+            out_dir.joinpath(file.name)
+        )
+        util_logger.debug('Run {}'.format(_cmd))
+
+        subprocess.check_output(_cmd, shell=True)
+
+    for file in chain(args, files):
+        _call_converter(file)
+        util_logger.info('{} was been converted to hccapx'.format(file.name))
+    else:
+        util_logger.error('No target provided for hccapx-converter')
+
+
+@add_perf_counter
+def merge_csv(out_dir: Path):
+    util_logger.debug('outdir: {}'.format(out_dir))
+    first_file, *tsv_files, last_file = filter(
+        lambda p: ('split' in p.name and p.name[-4:].lower() == '.tsv'),
+        out_dir.iterdir()
+    )
+
+    # split_Kismet-20170726-10-21-17-1-fixed_00000_20170711010628.tsv
+    # split_Kismet-20170725-09-35-21-1_00001_20170711010800.tsv
+    # subprocess.call ('"{}" -F pcap -c {} "{}" {}split_{}.pcap'.format(EDITCAP, chunk_size, inpcap, outdir, filename), shell=True)
+    # What's happening to them: -17-30-1-fixed-808-06-37-49-1.pcap.tsv
+
+    util_logger.debug("tsv_files: {}".format(tsv_files))
+    # Check to see if it has a split time
+
+    start_time = first_file.name.split('.')[0][-14:]
+    stop_time = last_file.name.split('.')[0][-14:]
+
+    out_file = Path('{}-{}.pcap.tsv'.format(start_time, stop_time))
+
+    with out_file.open('wb') as fout:
+        for f in tsv_files:
+            with f.open('rb') as fin:
+                # skip first line
+                _ = fin.readline()
+
+                fout.write(fin.read())
+
+    return out_file
